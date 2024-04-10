b3cdedca3c3106f06dd487dc7bebd6d993ebe044
FelipeDalpiva <felipedalpivadev@gmail.com>
2024-04-10 16:36:32 -0300

    improve: Addin try in events repository

--- src/models/entities/events.py
+++ src/models/entities/events.py
@@ -1,10 +1,11 @@
-from src.models.settings.base import base
+from src.models.settings.base import Base
 from sqlalchemy import Column, String, Integer
 
-class Events (base):
-  __tablename__ = "events"
-  id = Column(String, primary_key=True)
-  title = Column(String, nullable=False)
-  details = Column (String)
-  slug = Column(String, nullable=False)
-  maximun_attendees = Column (Integer)
+class Events(Base):
+    __tablename__ = "events"
+
+    id = Column(String, primary_key=True)
+    title = Column(String, nullable=False)
+    details = Column(String)
+    slug = Column(String, nullable=False)
+    maximum_attendees = Column(Integer)
